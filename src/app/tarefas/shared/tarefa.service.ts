62236c67fbdfb012955c7c40b1d4a1c2247d48fd
Marcos Buganeme <marcos.after@gmail.com>
2019-03-26 11:11:08 -0300

    adicionando regras de negocio das tarefas

--- src/app/tarefas/shared/tarefa.service.ts
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -9,8 +9,48 @@ export class TarefaService {
 
   constructor() { }
 
-  listarTodas(): Tarefa[] {
+  findAll(): Tarefa[] {
     const tarefas = localStorage['tarefas']
     return tarefas ? JSON.parse(tarefas) : []
+  }
+
+  save(tarefa: Tarefa): void {
+    const tarefas = this.findAll();
+    tarefa.id = new Date().getTime()
+    tarefas.push(tarefa)
+    localStorage['tarefas'] = JSON.stringify(tarefas)
+  }
+
+  findById(id: number): Tarefa {
+    const tarefas: Tarefa[] = this.findAll()
+    return tarefas.find(tarefa => tarefa.id === id)
+  }
+
+  update(tarefa: Tarefa): void {
+    const tarefas: Tarefa[] = this.findAll()
+    tarefas.forEach((obj, index, objs) => {
+      if (tarefa.id === obj.id) {
+        objs[index] = tarefa
+      }
+    })
+
+    localStorage['tarefas'] = JSON.stringify(tarefas)
+  }
+
+  delete(id: number): void {
+    let tarefas: Tarefa[] = this.findAll()
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id)
+    localStorage['tarefas'] = JSON.stringify(tarefas)
+  }
+
+  updateStatus(id: number): void {
+    const tarefas: Tarefa[] = this.findAll()
+    tarefas.forEach((obj, index, objs) => {
+      if (id === obj.id) {
+        objs[index].concluida = !obj.concluida
+      }
+    })
+
+    localStorage['tarefas'] = JSON.stringify(tarefas)
   }
 }
