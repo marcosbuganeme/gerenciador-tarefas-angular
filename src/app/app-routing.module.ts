a09ac1659def200773ea4b820e53084017b550ad
Marcos Buganeme <marcos.after@gmail.com>
2019-03-26 11:27:43 -0300

    registrando rotas de tarefas no modulo principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TarefaRoutes } from './tarefas/tarefas-routing.module'
+
 const routes: Routes = [
-  
+  {
+    path: '',
+    redirectTo: '/tarefas/listar',
+    pathMatch: 'full'
+  },
+  ...TarefaRoutes
 ];
 
 @NgModule({
